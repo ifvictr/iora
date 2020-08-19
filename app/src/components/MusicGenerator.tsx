20c65ad75397d8289beb741979248ac6d1829d34
Victor Truong <victor@victortruong.com>
2020-08-18 19:25:48 -0700

    Tweak beat durations

--- app/src/components/MusicGenerator.tsx
+++ app/src/components/MusicGenerator.tsx
@@ -16,14 +16,14 @@ interface Beat {
   duration: number
 }
 
-const CHARS_PER_THIRD = 93.333333
+const CHARS_PER_FOURTH = 70
 const getBeat = (payload: Payload): Beat => {
   // TODO: Get better notes/sequences with the variables in the payload
   const type = getEventType(payload)
   const tweetLength = payload.data.text.length
   return {
     note: NOTES[type],
-    duration: Math.ceil(tweetLength / CHARS_PER_THIRD) // 1–3 seconds
+    duration: Math.ceil(tweetLength / CHARS_PER_FOURTH)
   }
 }
 
@@ -31,20 +31,21 @@ const MusicGenerator = () => {
   const synthRef = useRef(new tone.Synth().toDestination())
   const beatQueueRef = useRef<Beat[]>([])
 
-  useEffect(() => {
-    const playIntervalId = setInterval(() => {
-      if (beatQueueRef.current.length === 0) {
-        return
-      }
-
-      const nextBeat = beatQueueRef.current.shift() as Beat
-      synthRef.current.triggerAttackRelease(nextBeat.note, nextBeat.duration)
-    }, 250)
-
-    return () => {
-      clearInterval(playIntervalId)
+  const checkForBeat = () => {
+    if (beatQueueRef.current.length === 0) {
+      setTimeout(checkForBeat, 1000) // Check for a new beat in one second
+      return
     }
-  }, [])
+
+    const nextBeat = beatQueueRef.current.shift() as Beat
+    synthRef.current.triggerAttackRelease(nextBeat.note, nextBeat.duration)
+
+    // Check for another beat to play after this one's time is up
+    setTimeout(checkForBeat, nextBeat.duration * 200)
+  }
+
+  // Start the check for beats to play on mount
+  useEffect(checkForBeat)
 
   useSocket('tweet', data => {
     const newPayload = JSON.parse(data) as Payload
