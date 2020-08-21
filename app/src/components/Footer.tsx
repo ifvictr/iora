2fa6b84be3c7322aacfa809dbe949fea640a07b9
Victor Truong <victor@victortruong.com>
2020-08-21 15:41:12 -0700

    Fix wrong author link in footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -5,13 +5,17 @@ const Footer = () => (
   <Container py={4}>
     <Text>
       Made by{' '}
-      <Link target="_blank" rel="noopener" href="https://victortruong.com">
+      <Link
+        target="_blank"
+        rel="author noopener"
+        href="https://victortruong.com"
+      >
         @ifvictr
       </Link>
       . See the code behind this on{' '}
       <Link
         target="_blank"
-        rel="author noopener"
+        rel="noopener"
         href="https://github.com/ifvictr/iora"
       >
         GitHub
